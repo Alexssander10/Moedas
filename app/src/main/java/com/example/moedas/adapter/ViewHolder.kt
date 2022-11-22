8a5bfe0548a2122bae29ac502aa750f5c392d2b2
Alexssander Albuquerque <alexssanderpro1@live.com>
2022-11-22 13:58:10 -0300

    Refactoring and changing the code to make it work better.

--- app/src/main/java/com/example/moedas/adapter/ViewHolder.kt
+++ app/src/main/java/com/example/moedas/adapter/ViewHolder.kt
@@ -12,10 +12,9 @@ class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val typeCurrency = itemView.findViewById<TextView>(R.id.type_Currency)
     private val currencyPercentage = itemView.findViewById<TextView>(R.id.currency_Percentage)
 
-    fun dadosMoeda(estruturaMoedas: ModelCurrency) {
-        typeCurrency.text = estruturaMoedas.isoMoeda
-        currencyPercentage.text = estruturaMoedas.currencyVariation.toString()
-        UtilityFunctions.colorVariation(currencyPercentage, estruturaMoedas)
+    fun dadosMoeda(currencyStruture: ModelCurrency) {
+        typeCurrency.text = currencyStruture.isoMoeda
+        currencyPercentage.text = currencyStruture.currencyVariation.toString()
+        UtilityFunctions.colorVariation(currencyPercentage, currencyStruture)
     }
-
 }
